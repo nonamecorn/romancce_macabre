a5aceabae92da61fb295f8c37eed49aa0f8cd524
corndev <corndev@mail.ru>
2025-09-27 21:14:48 +0300

    splitscreen start

---
+++ res_scripts/PlayerControls.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name PlayerControls #used for multiple players controls
+
+@export var player_index : int  = 0
+@export var move_left : String = "p1_moveleft" #this is action
+@export var move_right : String = "p1_moveright"
+@export var move_up : String = "p1_moveup"
+@export var move_down : String = "p1_movedown"
