cf390221c906c8b33e91400de22fc6bae2cf812b
corndev <corndev@mail.ru>
2025-10-02 17:42:05 +0300

    fixes

--- obj/drop_pod.gd
+++ obj/drop_pod.gd
@@ -3,8 +3,7 @@ extends StaticBody2D
 var drop_pod : StaticBody2D
 
 func load_pod(pod):
-	print("huh")
 	drop_pod = pod
 
 func teleport(body):
-	body.global_position = drop_pod.global_position + Vector2(0, 64)
+	body.global_position = drop_pod.global_position + Vector2(0, 32)
