133c58d1923aa621d477aa94030f97be23476b7d
corndev <corndev@mail.ru>
2025-10-02 12:39:28 +0300

    enemy hand stuff

--- obj/components/bodies/hand.gd
+++ obj/components/bodies/hand.gd
@@ -4,14 +4,14 @@ class_name EntityHand
 
 var flipped = false
 
+var pos := Vector2.ZERO
+
 func flip():
 	get_parent().flip()
 	flipped = !flipped
 	scale.y *= -1
 
 func _process(_delta: float) -> void:
-	var cursor_pos = get_global_mouse_position()
-	var pos = cursor_pos - global_position
 	global_rotation = atan2(pos.y, pos.x)
 	if pos.x < 0 and !flipped:
 		flip()
