d94d1b7d941aed1955ca2d1d2c460870fc065a1a
corndev <corndev@mail.ru>
2025-10-02 16:28:16 +0300

    enemy drops crops

--- obj/entity/enemies/enemydead_state.gd
+++ obj/entity/enemies/enemydead_state.gd
@@ -2,8 +2,13 @@ extends DeathState
 
 @export var perceptor : Perceptor
 @export var ray : RayCast2D
+@export var death_ingr : IngredientRes
 
 func enter():
 	super.enter()
 	perceptor.queue_free()
 	ray.queue_free()
+	var ingr = Ingredient.new()
+	ingr.res = death_ingr
+	ingr.global_position = entity.global_position
+	get_parent().get_parent().add_child(ingr)
