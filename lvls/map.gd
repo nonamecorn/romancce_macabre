ab8689d42b0df1af6b1419335c1756645665c633
corndev <corndev@mail.ru>
2025-10-02 20:25:14 +0300

    game over

--- lvls/map.gd
+++ lvls/map.gd
@@ -3,9 +3,9 @@ extends Node2D
 func finish():
 	Score.save_score()
 	if Score.score > 300:
-		$CanvasLayer/won.show()
+		$won.show()
 		$win.play()
 	else:
 		$loss.play()
-		$CanvasLayer/lost.show()
+		$lost.show()
 	get_tree().paused = true
